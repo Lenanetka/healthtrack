05a6693ee0dbab96c91cd3697c60c0eb20dee5c9
Lenanetka <LenaPaley@gmail.com>
2024-08-23 13:31:12 +0200

    Remove google fonts

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,48 +1,52 @@
 import 'palette.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 const appBarTheme = AppBarTheme(centerTitle: false, elevation: 0);
 
 ThemeData lightMode = ThemeData.light().copyWith(
-    primaryColor: PaletteLight.primaryColor,
-    scaffoldBackgroundColor: PaletteLight.backgroundColor,
-    appBarTheme: appBarTheme.copyWith(backgroundColor: PaletteLight.backgroundColor),
-    iconTheme: const IconThemeData(color: PaletteLight.contentColor),
-    textTheme: GoogleFonts.interTextTheme()
-        .apply(bodyColor: PaletteLight.contentColor),
-    colorScheme: const ColorScheme.light(
-      primary: PaletteLight.primaryColor,
-      secondary: PaletteLight.secondaryColor,
-      error: PaletteLight.errorColor,
-    ),
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      backgroundColor: PaletteLight.backgroundColor,
-      selectedItemColor: PaletteLight.contentColor.withOpacity(0.7),
-      unselectedItemColor: PaletteLight.contentColor.withOpacity(0.32),
-      selectedIconTheme: const IconThemeData(color: PaletteLight.primaryColor),
-      showUnselectedLabels: true,
-    ),
-  );
+  primaryColor: PaletteLight.primaryColor,
+  scaffoldBackgroundColor: PaletteLight.backgroundColor,
+  appBarTheme:
+      appBarTheme.copyWith(backgroundColor: PaletteLight.backgroundColor),
+  iconTheme: const IconThemeData(color: PaletteLight.contentColor),
+  textTheme: ThemeData.dark().textTheme.apply(
+        bodyColor: PaletteLight.contentColor,
+        displayColor: PaletteLight.contentColor,
+      ),
+  colorScheme: const ColorScheme.light(
+    primary: PaletteLight.primaryColor,
+    secondary: PaletteLight.secondaryColor,
+    error: PaletteLight.errorColor,
+  ),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    backgroundColor: PaletteLight.backgroundColor,
+    selectedItemColor: PaletteLight.contentColor.withOpacity(0.7),
+    unselectedItemColor: PaletteLight.contentColor.withOpacity(0.32),
+    selectedIconTheme: const IconThemeData(color: PaletteLight.primaryColor),
+    showUnselectedLabels: true,
+  ),
+);
 
 ThemeData darkMode = ThemeData.dark().copyWith(
-    primaryColor: PaletteDark.primaryColor,
-    scaffoldBackgroundColor: PaletteDark.backgroundColor,
-    appBarTheme: appBarTheme.copyWith(backgroundColor: PaletteDark.backgroundColor),
-    iconTheme: const IconThemeData(color: PaletteDark.contentColor),
-    textTheme: GoogleFonts.interTextTheme()
-        .apply(bodyColor: PaletteDark.contentColor),
-    colorScheme: const ColorScheme.dark().copyWith(
-      primary: PaletteDark.primaryColor,
-      secondary: PaletteDark.secondaryColor,
-      error: PaletteDark.errorColor,
-    ),
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      backgroundColor: PaletteDark.backgroundColor,
-      selectedItemColor: PaletteDark.contentColor.withOpacity(0.7),
-      unselectedItemColor: PaletteDark.contentColor.withOpacity(0.32),
-      selectedIconTheme: const IconThemeData(color: PaletteDark.primaryColor),
-      showUnselectedLabels: true,
-    ),
-  );
-
+  primaryColor: PaletteDark.primaryColor,
+  scaffoldBackgroundColor: PaletteDark.backgroundColor,
+  appBarTheme:
+      appBarTheme.copyWith(backgroundColor: PaletteDark.backgroundColor),
+  iconTheme: const IconThemeData(color: PaletteDark.contentColor),
+  textTheme: ThemeData.dark().textTheme.apply(
+        bodyColor: PaletteDark.contentColor,
+        displayColor: PaletteDark.contentColor,
+      ),
+  colorScheme: const ColorScheme.dark().copyWith(
+    primary: PaletteDark.primaryColor,
+    secondary: PaletteDark.secondaryColor,
+    error: PaletteDark.errorColor,
+  ),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    backgroundColor: PaletteDark.backgroundColor,
+    selectedItemColor: PaletteDark.contentColor.withOpacity(0.7),
+    unselectedItemColor: PaletteDark.contentColor.withOpacity(0.32),
+    selectedIconTheme: const IconThemeData(color: PaletteDark.primaryColor),
+    showUnselectedLabels: true,
+  ),
+);
