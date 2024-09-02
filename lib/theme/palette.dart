52b0495b769e2236ef2a13695be419217a017865
Lenanetka <LenaPaley@gmail.com>
2024-09-02 23:03:11 +0200

    Change secondary color to white

--- lib/theme/palette.dart
+++ lib/theme/palette.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class PaletteLight {
   static const primaryColor = Color(0xFF00BF6D);
-  static const secondaryColor = Color(0xFFFE9901);
+  static const secondaryColor = Colors.white;
   static const backgroundColor = Colors.white;
   static const contentColor = Color(0xFF1D1D35);
   static const warninngColor = Color(0xFFF3BB1C);
@@ -11,7 +11,7 @@ class PaletteLight {
 
 class PaletteDark {
   static const primaryColor = Color(0xFFA34ED8);
-  static const secondaryColor = Color(0xFFFE9901);
+  static const secondaryColor = Colors.white;
   static const backgroundColor = Color(0xFF303030);
   static const contentColor = Color(0xFFE0E0E0);
   static const warninngColor = Color(0xFFF3BB1C);
